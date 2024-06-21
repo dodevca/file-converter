31a8ebe73c86699b8653520a886eaba03cae7787
siwanyesarela <128023797+siwanyesarela@users.noreply.github.com>
2024-06-21 14:11:52 +0700

    Update profile.php

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -2,40 +2,60 @@
 
 <?= $this->section('main') ?>
 <div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <h2>Account Info</h2>
-      <form>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" class="form-control" id="email" value="twelvedotio@gmail.com">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Account Info</h2>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="twelvedotio@gmail.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Change password">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2>Billing Info</h2>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Rio Ananda</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Rio Ananda">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Tax/VAT ID</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Tax/VAT ID">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Sinduadi</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Sinduadi">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Mlati</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mlati">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Sleman</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Sleman">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Yogyakarta</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Yogyakarta">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlSelect1" class="form-label">Indonesia</label>
+                    <select class="form-select" id="exampleFormControlSelect1">
+                        <option value="indonesia">Indonesia</option>
+                        <option value="malaysia">Malaysia</option>
+                        <option value="singapore">Singapore</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Update Billing Info</button>
+            </div>
         </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password">
-          <a href="#" class="text-muted">Change password</a>
-        </div>
-        <div class="form-group">
-          <label for="billing-info">Billing Info</label>
-          <input type="text" class="form-control" id="billing-info" value="Rio Ananda">
-        </div>
-        <div class="form-group">
-          <label for="tax-vat-id">Tax/VAT ID</label>
-          <input type="text" class="form-control" id="tax-vat-id" value="Sinduadi">
-        </div>
-        <div class="form-group">
-          <label for="address">Address</label>
-          <input type="text" class="form-control" id="address" value="Mlati, Sleman, Yogyakarta, Indonesia">
-        </div>
-        <div class="form-group">
-          <label for="postal-code">Postal Code</label>
-          <input type="text" class="form-control" id="postal-code" value="55284">
-        </div>
-        <button type="submit" class="btn btn-primary">Update Billing Info</button>
-      </form>
     </div>
-  </div>
-</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
+
 <?= $this->endSection() ?>
 
 <?= $this->section('script') ?>
