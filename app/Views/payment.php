0ed9d94a97488e8a6bbb20d00efb2ac907b28adc
RolandSitompul <128023772+RolandSitompul750@users.noreply.github.com>
2024-06-21 14:08:26 +0700

    Update payment.php

--- app/Views/payment.php
+++ app/Views/payment.php
@@ -1,7 +1,28 @@
 <?= $this->extend('layouts/dashboard') ?>
 
 <?= $this->section('main') ?>
-
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">INVOICE DATE</th>
+      <th scope="col">INVOICE ID</th>
+      <th scope="col">AMOUNT</th>
+      <th scope="col">STATUS</th>
+      <th scope="col">METHOD</th>
+      <th scope="col">DOWNLOAD</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">2024-06-19T00:58:06:201Z</th>
+      <td>66722D1e897595c2ddb4c00</td>
+      <td>8</td>
+      <td>Paid</td>
+      <td>Stripe</td>
+      <td><button type="button" class="btn btn-outline-secondary">PDF</button></td>
+    </tr>
+  </tbody>
+</table>
 <?= $this->endSection() ?>
 
 <?= $this->section('script') ?>
