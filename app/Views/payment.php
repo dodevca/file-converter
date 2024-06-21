4f9e9727ff586be1a298d554a2b3a8ca4f5182c8
RolandSitompul <128023772+RolandSitompul750@users.noreply.github.com>
2024-06-21 14:18:04 +0700

    Update payment.php

--- app/Views/payment.php
+++ app/Views/payment.php
@@ -5,9 +5,9 @@
   <table class="table">
   <thead>
     <tr>
-      <th scope="col">INVOICE DATE</th>
-      <th scope="col">INVOICE ID</th>
-      <th scope="col">AMOUNT</th>
+      <th scope="col">TANGGAL</th>
+      <th scope="col">TOKEN</th>
+      <th scope="col">HARGA</th>
       <th scope="col">STATUS</th>
       <th scope="col">METHOD</th>
       <th scope="col">DOWNLOAD</th>
@@ -18,7 +18,7 @@
       <th scope="row">2024-06-19T00:58:06:201Z</th>
       <td>66722D1e897595c2ddb4c00</td>
       <td>8</td>
-      <td>Paid</td>
+      <td>LUNAS</td>
       <td>Stripe</td>
       <td><button type="button" class="btn btn-outline-secondary">PDF</button></td>
     </tr>
