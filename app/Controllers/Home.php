225955693a48f3e34437154efa2759aa0f216010
Dodevca <twelvedotio@gmail.com>
2024-07-02 21:40:24 +0800

    Update sign up validation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -98,7 +98,8 @@ class Home extends BaseController
                 'rules'     => 'required|valid_email|is_unique[pengguna.email]',
                 'errors'    => [
                     'required'      => 'Email tidak boleh kosong. Masukkan email yang terdaftar',
-                    'valid_email'   => 'Email tidak terdaftar. Pastikan penulisan format email benar'
+                    'valid_email'   => 'Email tidak terdaftar. Pastikan penulisan format email benar',
+                    'is_unique'     => 'Email sudah terdaftar'
                 ]
             ],
             'password'  => [
